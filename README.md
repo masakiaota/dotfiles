1ffa8f0177e0b7b4424855cc67adb7d151d78325
Masaki AOTA <aotamasakimail@gmail.com>
2018-08-06 09:43:41 +0900

    add generated color settings(by fish)(my favorite). modify README

--- README.md
+++ README.md
@@ -13,12 +13,9 @@ ln -s ~/dotfiles/vscode/settings.json
 
 fishの設定(fish/にあるlink.shで実行可能)
 ```
-cd ~/.config/fish
-ln -s ~/dotfiles/fish/config.fish
-cd functions
-ln -s ~/dotfiles/fish/functions/fish_prompt.fish
-ln -s ~/dotfiles/fish/functions/fish_user_key_bindings.fish
-ln -s ~/dotfiles/fish/functions/cd.fish
+cd ~/.config/
+mv fish/ fish.bak/
+ln -s ~/dotfiles/fish
 ```
 
 vim
--- fish/link.sh
+++ fish/link.sh
@@ -1,7 +1,8 @@
-#to link fish settings
-cd ~/.config/fish
-ln -s ~/dotfiles/fish/config.fish
-cd functions
-ln -s ~/dotfiles/fish/functions/fish_prompt.fish
-ln -s ~/dotfiles/fish/functions/fish_user_key_bindings.fish
-#ln -s ~/dotfiles/fish/functions/cd.fish
+# #これを実行するな、ファイルごとlinkできるようにした
+# #to link fish settings
+# cd ~/.config/fish
+# ln -s ~/dotfiles/fish/config.fish
+# cd functions
+# ln -s ~/dotfiles/fish/functions/fish_prompt.fish
+# ln -s ~/dotfiles/fish/functions/fish_user_key_bindings.fish
+# #ln -s ~/dotfiles/fish/functions/cd.fish
