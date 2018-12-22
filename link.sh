b7a151d31a0b74e0574a674473857fc87742486f
MasakiAOTA <aotamasakimail@gmail.com>
2018-12-22 12:53:39 +0900

    link -s karabiner

--- link.sh
+++ link.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
 
+# karabiner
+echo "setting karabiner ? (yes/no)"
+read ans
+case $ans in
+    yes)
+    cd ~/.config
+    mv karabiner karabiner.bak
+    ln -s ~/dotfiles/karabiner
+    ;;
+    *)
+    echo "skip";;
+esac
+
+echo
+
 #bashの設定
 echo "setting bash"
 cd
 mv .bashrc bashrc.old
 ln -s ~/dotfiles/bash/.bashrc
+
+echo
 
 #fishの設定
 echo "setting fish..."
@@ -15,36 +32,50 @@ ln -s ~/dotfiles/fish/config.fish
 ln -s ~/dotfiles/fish/functions
 # cp fish.bak/fishd.* ~/dotfiles/fish/ #既存の設定の継承
 
+echo
 #hyper
 # echo "setting hyper..."
 # cd
 # ln -sf ~/dotfiles/.hyper.js
 
+echo
 #xonsh
 echo "setting xonsh..."
 cd
 ln -s ~/dotfiles/.xonshrc
+
+echo
 
 #tmux
 echo "setting tmux..."
 cd
 ln -s ~/dotfiles/.tmux.conf
 
+echo
+
 #vim
 echo "setting vim..."
 cd
 ln -s ~/dotfiles/.vimrc
 
-#vscode
-echo "setting vscode..."
-if [ -e ~/Library/Application\ Support/Code/User ]; then
-    # 存在する場合
-    cd ~/Library/Application\ Support/Code/User
-    ln -s ~/dotfiles/vscode/keybindings.json
-    ln -s ~/dotfiles/vscode/settings.json
-else
-    # 存在しない場合
-    echo "vscode has not been installed"
-fi
+echo
 
+#vscode
+echo "setting vscode ? (yes/no)"
+read ans
+case $ans in
+    yes)
+    if [ -e ~/Library/Application\ Support/Code/User ]; then
+        # 存在する場合
+        cd ~/Library/Application\ Support/Code/User
+        ln -s ~/dotfiles/vscode/keybindings.json
+        ln -s ~/dotfiles/vscode/settings.json
+    else
+        # 存在しない場合
+        echo "vscode has not been installed"
+    fi
+    ;;
+    *)
+    echo "skip";;
+esac
 echo "Please refer to README.md, in orderto activate some extensions."
