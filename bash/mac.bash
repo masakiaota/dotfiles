78598d88968901d121f4b3b1f3e53c0ff3bf1b9a
MasakiAOTA <aotamasakimail@gmail.com>
2019-03-27 18:13:11 +0900

    available only in interactive mode

--- bash/mac.bash
+++ bash/mac.bash
@@ -1,21 +1,29 @@
 #環境変数
 export PATH="/bin/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
-# for bash_completion
-if [ -f `brew --prefix`/etc/bash_completion ]; then
-  source `brew --prefix`/etc/bash_completion
-fi
+
+case $- in
+    *i*)#インタラクティブモードの処理をこの中に書く。
+
+        # for bash_completion
+        if [ -f `brew --prefix`/etc/bash_completion ]; then
+          source `brew --prefix`/etc/bash_completion
+        fi
 
 
-#pyenv
-echo "starting pyenv"
-export PYENV_ROOT="$HOME/.pyenv"
-if [ -d ${PYENV_ROOT} ]; then
-  export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
-  if [ -d ${PYENV_ROOT}/plugins/pyenv-virtualenv ]; then
-    eval "$(pyenv virtualenv-init -)"
-  fi
-fi
+        #pyenv
+        echo "starting pyenv"
+        export PYENV_ROOT="$HOME/.pyenv"
+        if [ -d ${PYENV_ROOT} ]; then
+          export PATH="$PYENV_ROOT/bin:$PATH"
+          eval "$(pyenv init -)"
+          if [ -d ${PYENV_ROOT}/plugins/pyenv-virtualenv ]; then
+            eval "$(pyenv virtualenv-init -)"
+          fi
+        fi
+
+        ;;
+      #*) return;;
+esac
 
 # alias
 
