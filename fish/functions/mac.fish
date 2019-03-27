cdacd61b056aecd82abeeb7cf94949638286283f
MasakiAOTA <aotamasakimail@gmail.com>
2019-03-27 21:34:39 +0900

    recover pyenv activation in fish

--- fish/functions/mac.fish
+++ fish/functions/mac.fish
@@ -1,22 +1,14 @@
-#bash経由で起動するようにするのでここらへんの設定が不要になった
-#起動の高速化のため、python3と打ったらデフォルトをpython3に変更。以降pythonで3が起動する。
-# function p3
-#     set PYENV_ROOT $HOME/.pyenv
 
-#     # なぜかMBAにはbinがないのに動く(エラーがうざいので空でmkdirした)
-#     set PATH $PYENV_ROOT/bin $PATH
-#     . (pyenv init - | psub);
-#     . (pyenv virtualenv-init - | psub); and python -V;
-#     which python;
-# end
+#本当はintaractiveモードのときだけ実行するというふうに書いたほうがいいのだろうが。
+set -x PYENV_ROOT $HOME/.pyenv
 
-# function notebook
-#     set PYENV_ROOT $HOME/.pyenv
-#     # なぜかMBAにはbinがないのに動く(エラーがうざいので空でmkdirした)
-#     set PATH $PYENV_ROOT/bin $PATH
-#     . (pyenv init - | psub);
-#     . (pyenv virtualenv-init - | psub); and jupyter notebook $argv
-# end
+# なぜかMBAにはbinがないのに動く(エラーがうざいので空でmkdirした)
+set -x PATH $PYENV_ROOT/bin $PATH
+. (pyenv init - | psub);
+. (pyenv virtualenv-init - | psub);
+echo 'your python env is'
+pyenv versions;
+
 
 #alias
 ## rmでゴミ箱に入れる
