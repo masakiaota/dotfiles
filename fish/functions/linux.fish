d2a5f1e2a169f225990a382c0b277e51efd4172d
MasakiAota <aotamasakimail@gmail.com>
2019-06-20 15:00:11 +0900

    change pyenv settings

--- fish/functions/linux.fish
+++ fish/functions/linux.fish
@@ -8,7 +8,7 @@ set -x PATH $PYENV_ROOT/bin $PATH
 . (pyenv virtualenv-init - | psub);
 
 echo "pyenv activate anaconda3-4.3.1 for ubuntu"
-pyenv activate anaconda3-4.3.1
+pyenv global anaconda3-4.3.1
 
 echo 'your python env is'
 pyenv versions;
