a8fa637e2a768f00cced470c47423be16d1271cb
Masaki AOTA <41673718+masakiaota@users.noreply.github.com>
2025-05-07 12:38:14 +0900

    Update cuda-keyring in setup.sh

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -116,8 +116,8 @@ set -g @continuum-restore 'on'
 # https://docs.nvidia.com/datacenter/tesla/tesla-installation-notes/index.html
 sudo apt-get install linux-headers-$(uname -r)
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 
